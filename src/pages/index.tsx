9f51624b9507c80336efdac03095950d0317e3f7
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-01 16:00:18 -0300

    created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
     <>
@@ -7,7 +9,30 @@ export default function Home() {
         <title>Board - Organize seu dia</title>
       </Head>
 
-      <h1>Home</h1>
+      <main className={styles.content}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.banner}>
+          <h1>
+            Uma ferramenta para seu dia a dia, Escreva, planeje e organize-se...
+          </h1>
+
+          <p>
+            <span>100% Gratuita</span> e online.
+          </p>
+        </section>
+
+        <div className={styles.donates}>
+          <span>Apoiadores</span>
+
+          <div className={styles.listDonates}>
+            <img
+              src="https://avatars.githubusercontent.com/u/36782514?v=4"
+              alt="Calebe"
+            />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
