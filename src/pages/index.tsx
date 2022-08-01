926c714b314aa2f34296dad3586df2bb9df5a495
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-01 08:56:34 -0300

    config project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Board - Organize seu dia</title>
+      </Head>
+
+      <h1>Home</h1>
+    </>
+  )
+}
