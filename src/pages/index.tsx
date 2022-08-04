1b4546f6df6c982904e85390214af10b52b3646f
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-04 12:57:51 -0300

    generate static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import styles from '../styles/home.module.scss'
@@ -35,4 +36,11 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+    revalidate: 60 * 60, // 1h
+  }
 }
