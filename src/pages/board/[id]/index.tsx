72450e777eb0f9d9b396c208451d418e66e2371b
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-06 08:50:46 -0300

    created page detail

---
+++ src/pages/board/[id]/index.tsx
@@ -0,0 +1,71 @@
+import { format } from 'date-fns'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
+import Head from 'next/head'
+import { FiCalendar } from 'react-icons/fi'
+
+import { Task } from '../../../components/Task'
+import firebase from '../../../services/firebaseConfig'
+
+import styles from './styles.module.scss'
+
+interface DetailProps {
+  data: string
+}
+
+export default function Detail({ data }: DetailProps) {
+  const task = JSON.parse(data) as Task
+
+  return (
+    <>
+      <Head>
+        <title>Task detail</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.card}>
+          <header>
+            <FiCalendar />
+            <span>
+              Tarefa criada: <time>{task.createdFormatted}</time>
+            </span>
+          </header>
+          <p>{task.task}</p>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req })
+  const id = params?.id as string
+
+  if (!session?.id) {
+    return {
+      redirect: {
+        destination: '/board',
+        permanent: false,
+      },
+    }
+  }
+
+  const snapshot = await firebase.firestore().collection('tasks').doc(id).get()
+  const task = {
+    created: snapshot.data()?.created,
+    id: snapshot.id,
+    task: snapshot.data()?.task,
+    createdFormatted: format(snapshot.data()?.created.toDate(), 'dd MMM yyyy'),
+    userId: snapshot.data()?.userId,
+    username: snapshot.data()?.username,
+  }
+
+  return {
+    props: {
+      data: JSON.stringify(task),
+    },
+  }
+}
