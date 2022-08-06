70eeb00426e59bb33ef57b435b513e8b124f8850
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-06 14:38:51 -0300

    paypal integrated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { Header } from '../components/Header'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const options = {
+    'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID as string,
+    currency: 'BRL',
+    intent: 'capture',
+  }
+
   return (
     <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
+      <PayPalScriptProvider options={options}>
+        <Header />
+        <Component {...pageProps} />
+      </PayPalScriptProvider>
     </SessionProvider>
   )
 }
